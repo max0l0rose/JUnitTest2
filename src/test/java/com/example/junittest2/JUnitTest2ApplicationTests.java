686895758ac3846e6ec0f5451bbf24ef15498f9d
9956799@gmail.com <Qqq_12345>
2022-07-10 20:43:36 +0300

    @ConditionalOnProperty

--- src/test/java/com/example/junittest2/JUnitTest2ApplicationTests.java
+++ src/test/java/com/example/junittest2/JUnitTest2ApplicationTests.java
@@ -1,31 +1,50 @@
 package com.example.junittest2;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(classes = {Conf.class, Conf2.class
-})
+//@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {Conf.class//, Conf2.class
+}
+		,properties = "io.reflectoring.scheduling.enabled=true"
+)
+
+//@ActiveProfiles("test")
+//@TestPropertySource(locations = "/foo.properties")
+
+//@Import(JUnitTest2ApplicationTests.TestConfig.class)
+//@ContextHierarchy({
+	@ContextConfiguration(classes = JUnitTest2ApplicationTests.TestConfig.class)
+//})
+//@AutoConfigureMockMvc
 class JUnitTest2ApplicationTests {
 
-	@Bean
-	@Primary
-	int iii() {
-		System.out.println("iiiTest()");
-		return 1;
-	}
-
+	//@EnableScheduling
+	@ConditionalOnProperty(
+			name = "io.reflectoring.scheduling.enabled",
+			havingValue = "true",
+			matchIfMissing = true)
 	@TestConfiguration
 	static class TestConfig {
-
 //		@Bean
 //		public RestTemplateBuilder restTemplateBuilder() {
 //
@@ -35,13 +54,14 @@ class JUnitTest2ApplicationTests {
 //		}
 
 		@Bean
-		@Primary
+			//@Primary
 		int iii() {
 			System.out.println("iiiTest()");
 			return 1;
 		}
-
 	}
+	//@TestConfiguration
+	//@Configuration
 
 	@Autowired
 	List<String> list;
