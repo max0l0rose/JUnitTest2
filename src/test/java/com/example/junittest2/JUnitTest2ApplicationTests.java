ebc83b9d0325cbcd5634c4616464c050ca484d01
9956799@gmail.com <Qqq_12345>
2022-07-10 16:22:17 +0300

    @TestConfiguration

--- src/test/java/com/example/junittest2/JUnitTest2ApplicationTests.java
+++ src/test/java/com/example/junittest2/JUnitTest2ApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.junittest2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
@@ -10,14 +12,35 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(classes = {Conf.class//, Conf2.class
+@SpringBootTest(classes = {Conf.class, Conf2.class
 })
 class JUnitTest2ApplicationTests {
 
 	@Bean
+	@Primary
 	int iii() {
 		System.out.println("iiiTest()");
 		return 1;
+	}
+
+	@TestConfiguration
+	static class TestConfig {
+
+//		@Bean
+//		public RestTemplateBuilder restTemplateBuilder() {
+//
+//			return new RestTemplateBuilder()
+//					.basicAuthentication("mkyong", "password")
+//					.setConnectTimeout(Duration.ofSeconds(5));
+//		}
+
+		@Bean
+		@Primary
+		int iii() {
+			System.out.println("iiiTest()");
+			return 1;
+		}
+
 	}
 
 	@Autowired
@@ -29,7 +52,7 @@ class JUnitTest2ApplicationTests {
 
 	@Test
 	void strResultTest() {
-		assertEquals(list.get(0), "Qqq");
+		assertEquals("str", list.get(0));
 	}
 
 }
